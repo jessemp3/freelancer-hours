2203772de34d3302a57579f44935c44a76ec9ea6
jesse <nitchsx@gmail.com>
2024-10-09 18:53:39 -0300

    Primeira aula de Php

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+
+
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/' , WelcomeController::class);
